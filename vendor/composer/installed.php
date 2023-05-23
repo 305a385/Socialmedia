964bfb2b20feb02e09992b7207df1d640780bfcb
305a385 <132265158+ef3dd1b07c14731a7fe2fd5455e50fc3@users.noreply.github.com>
2023-05-23 21:41:28 +0200

    update composer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => 'a6ca39f896112ee3a8ceac08d77ca5770a2f957a',
+        'reference' => '219ff4bc8062ea627f8a4f44f0a8c3afe34b6b8b',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => 'a6ca39f896112ee3a8ceac08d77ca5770a2f957a',
+            'reference' => '219ff4bc8062ea627f8a4f44f0a8c3afe34b6b8b',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
